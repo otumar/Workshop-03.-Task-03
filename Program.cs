fd23d91ad7fce74190563fa0c6496c585c5fbbf8
«Тумар <otumar@yandex.ru>
2022-06-20 00:03:30 +0300

    Task 03

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+int Prompt(string massage)
+{
+    System.Console.Write(massage);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+int number = Prompt("Введите число: ");
+int count = 0;
+while (count < number)
+{
+count++;
+System.Console.WriteLine(count*count*count);
+}
